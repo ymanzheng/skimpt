4688db69de8c7b310f3b3e40480745585ac8a0e2
affan05 <affan05@b83dcf14-a17d-11dd-a9c7-49dec45ca499>
2008-11-02 02:59:10 +0000

    Created new lowlevel Keyboard hook, that hooks the printscreen button. Added empty viewfinder Added form event functions to main.cs Deleted hotkey.dll

---
+++ Platters/classes/keyboardhook.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+public delegate void KeyboardHookCaptureHandler();
+
+
+public class KeyboardHook
+{
+    public event KeyboardHookCaptureHandler KeyIntercepted;
+
+    private const int WH_KEYBOARD_LL = 13;
+    private const int WM_KEYDOWN = 0x0100;
+    private LowLevelKeyboardProc _proc;
+    private IntPtr _hookID = IntPtr.Zero;
+
+    public KeyboardHook()
+    {
+         _proc = HookCallback;
+         _hookID = SetHook(_proc);
+    }
+    public bool UnHookKey()
+    {
+        return UnhookWindowsHookEx(_hookID);
+    }
+
+    private IntPtr SetHook(LowLevelKeyboardProc proc)
+    {
+        using (Process curProcess = Process.GetCurrentProcess())
+        using (ProcessModule curModule = curProcess.MainModule)
+        {
+            return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                GetModuleHandle(curModule.ModuleName), 0);
+        }
+    }
+
+    private delegate IntPtr LowLevelKeyboardProc(
+    int nCode, IntPtr wParam, IntPtr lParam);
+
+    private IntPtr HookCallback(
+        int nCode, IntPtr wParam, IntPtr lParam)
+    {
+        if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+        {
+            int vkCode = Marshal.ReadInt32(lParam);
+            //Console.WriteLine((Keys)vkCode);
+            if ((Keys)vkCode == Keys.PrintScreen)
+            {
+                RaiseEvent();
+            }                
+        }
+        return CallNextHookEx(_hookID, nCode, wParam, lParam);
+    }
+
+    private void RaiseEvent()
+    {
+        KeyIntercepted();
+
+    }
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr SetWindowsHookEx(int idHook,
+        LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+        IntPtr wParam, IntPtr lParam);
+
+    [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr GetModuleHandle(string lpModuleName);
+}
