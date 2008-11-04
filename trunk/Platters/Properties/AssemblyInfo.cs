da85ce74f2a2695dc9987aea36b20f940a919139
sid78669 <sid78669@b83dcf14-a17d-11dd-a9c7-49dec45ca499>
2008-11-04 21:25:08 +0000

    Added the Skimpt Icon and more settings for the setup.

--- trunk/Platters/Properties/AssemblyInfo.cs
+++ trunk/Platters/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Platters")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Skimpy")]
+[assembly: AssemblyDescription("Screen Shot taking Utility")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Platters")]
+[assembly: AssemblyProduct("Skimpt")]
 [assembly: AssemblyCopyright("Copyright ©  2008")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
