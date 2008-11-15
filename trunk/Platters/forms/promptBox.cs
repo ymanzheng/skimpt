1cc583596b907d205933fc4aae4933dd06c60a30
sid78669 <sid78669@b83dcf14-a17d-11dd-a9c7-49dec45ca499>
2008-11-15 09:23:49 +0000

    Changed text for the Unhook Print Screen Key. Added new form template, can be used any where as all the fields, buttons and labels are public. Added new setting to check if the fileSettingsLocation has been entered or not, useful for first runs. Setting for the check is not fully complete and requires to be worked on, but the prompt is working (main.cs).

---
+++ trunk/Platters/forms/promptBox.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Skimpt.forms
+{
+    public partial class promptBox : Form
+    {
+        public promptBox()
+        {
+            InitializeComponent();
+        }
+
+        public void promptBox_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void promptBoxLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
